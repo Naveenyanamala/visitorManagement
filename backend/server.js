1301bdfb9899b756fb1fa3071922a3fcb10fb51b
Naveenyanamala <navinyanamlla@gmail.com>
2025-10-20 20:02:14 +0530

    - removed listen

--- backend/server.js
+++ backend/server.js
@@ -137,10 +137,10 @@ console.log('Connected to MongoDB');
 //  next();
 // });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
